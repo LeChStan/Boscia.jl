76e4014bd10fc7979007195061ec67d4044766e3
Leon <stanzel.leon@gmail.com>
2024-10-02 17:31:55 +0200

    Nonlinear example

--- examples/nonlinear.jl
+++ examples/nonlinear.jl
@@ -1,118 +1,75 @@
-using FrankWolfe
-using LinearAlgebra
-import MathOptInterface
-using Random
 using Boscia
-using Bonobo
-import Bonobo
-using Printf
-using Dates
-
-# using SCIP
-# const MOI = MathOptInterface
-
-n = 40
-seed = 10
-
-Random.seed!(seed)
-
-################################################################
-# alternative implementation of LMO using MOI and SCIP
-################################################################
-# o = SCIP.Optimizer()
-# MOI.set(o, MOI.Silent(), true)
-# MOI.empty!(o)
-# x = MOI.add_variables(o, n)
-# for xi in x
-#     MOI.add_constraint(o, xi, MOI.ZeroOne())
-#     MOI.add_constraint(o, xi, MOI.GreaterThan(0.0))
-#     MOI.add_constraint(o, xi, MOI.LessThan(1.0))
-# end
-# lmo = FrankWolfe.MathOptLMO(o)
+using FrankWolfe
+using Test
+using Random
+using SCIP
+using HiGHS
+# using Statistics
+using LinearAlgebra
+using Distributions
+using CSV
+using DataFrames
+import MathOptInterface
+const MOI = MathOptInterface
+include("example_utilities.jl")
 
 
-################################################################
-# LMO via CubeSimpleBLMO
-################################################################
-int_vars = collect(1:n)
 
-lbs = zeros(n)
-ubs = ones(n)
 
-sblmo = Boscia.CubeSimpleBLMO(lbs, ubs, int_vars)
-# wrap the sblmo into a bound manager
-lmo = Boscia.ManagedBoundedLMO(sblmo, lbs[int_vars], ubs[int_vars], int_vars, n)
+function build_examples(o, n,  seed)
+    Random.seed!(seed)
+    A = let
+        A = randn(n, n)
+        A' * A
+    end
+    
+    @assert isposdef(A) == true
+    
+    y = Random.rand(Bool, n) * 0.6 .+ 0.3
+    
+    MOI.set(o, MOI.Silent(), true)
+    MOI.empty!(o)
+    x = MOI.add_variables(o, n)
+    for xi in x
+        MOI.add_constraint(o, xi, MOI.ZeroOne())
+        MOI.add_constraint(o, xi, MOI.GreaterThan(0.0))
+        MOI.add_constraint(o, xi, MOI.LessThan(1.0))
+    end
+    lmo = Boscia.MathOptBLMO(o)
 
-const A = let
-    A = randn(n, n)
-    A' * A
+    function f(x)
+        d = x - y
+        return dot(d, A, d)
+    end
+
+    function grad!(storage, x)
+        # storage = Ax
+        mul!(storage, A, x)
+        # storage = 2Ax - 2Ay
+        return mul!(storage, A, y, -2, 2)
+    end
+    return f, grad!, lmo
+end
+   
+
+function nonlinear_boscia(mode, dimension, seed, alternative, decision_function, iterations_until_stable, μ,  time_limit = 1800)
+    o = SCIP.Optimizer()
+    f, grad!, lmo = build_examples(o, dimension,  seed)
+    branching_strategy, settings = build_branching_strategy(lmo, mode, alternative, decision_function, iterations_until_stable, μ)
+    # println(o)
+    println("precompile")
+    Boscia.solve(f, grad!, lmo, verbose=false, time_limit=10)
+   # print(lmo.o)
+    println("actual run")
+
+    x, _, result = Boscia.solve(f, grad!, lmo, verbose=true, time_limit=time_limit, branching_strategy = branching_strategy)
+
+    ### Define Parameters needed for documenting the results ###
+    example_name = "nonlinear"
+    file_name = "nonlinear_results"
+    # Save result
+    save_results(result, settings, μ, example_name, seed, dimension, file_name,false)
+    return result
 end
 
-@assert isposdef(A) == true
-
-const y = Random.rand(Bool, n) * 0.6 .+ 0.3
-
-function f(x)
-    d = x - y
-    return dot(d, A, d)
-end
-
-function grad!(storage, x)
-    # storage = Ax
-    mul!(storage, A, x)
-    # storage = 2Ax - 2Ay
-    return mul!(storage, A, y, -2, 2)
-end
-
-# benchmarking Oracles
-FrankWolfe.benchmark_oracles(f, grad!, () -> rand(n), lmo; k=100)
-
-#################
-
-
-# are these lmo calls counted as well?
-
-# #####
-# # follow the gradient for a fixed number of steps and collect solutions on the way
-# #####
-
-# function follow_gradient_heuristic(tree::Bonobo.BnBTree, blmo::Boscia.BoundedLinearMinimizationOracle, x, k)
-#     nabla = similar(x)
-#     x_new = copy(x)
-#     sols = []
-#     for i in 1:k
-#         tree.root.problem.g(nabla,x_new)
-#         x_new = Boscia.compute_extreme_point(blmo, nabla)
-#         push!(sols, x_new)
-#     end
-#     return sols, false
-# end
-
-# #####
-# # rounding respecting the hidden feasible region structure
-# #####
-
-# function rounding_lmo_01_heuristic(tree::Bonobo.BnBTree, blmo::Boscia.BoundedLinearMinimizationOracle, x)
-#     nabla = zeros(length(x))
-#     for idx in tree.branching_indices
-#         nabla[idx] = 1 - 2*round(x[idx]) # (0.7, 0.3) -> (1, 0) -> (-1, 1) -> min -> (1,0)
-#     end
-#     x_rounded = Boscia.compute_extreme_point(blmo, nabla)
-#     return [x_rounded], false
-# end
-
-#####
-# geometric scaling like for a couple of steps
-#####
-
-
-depth = 5
-heu  = Boscia.Heuristic((tree, blmo, x) -> Boscia.follow_gradient_heuristic(tree,blmo,x, depth), 0.8, :follow_gradient)
-heu2 = Boscia.Heuristic(Boscia.rounding_lmo_01_heuristic, 0.8, :lmo_rounding)
-
-heuristics = [heu, heu2]
-# heuristics = []
-
-x, _, _ = Boscia.solve(f, grad!, lmo, verbose=true, print_iter=500, custom_heuristics=heuristics)
-
-@show x
+nonlinear_boscia("pseudocost", 20, 2, "most_infeasible", "product", 4, 1e-6,  600)
---
+++ examples/run_nonlinear_setup.jl
@@ -0,0 +1,23 @@
+include("nonlinear.jl")
+
+################# Read ARGS passed #######################
+mode = parse(String, ARGS[1])
+dimension = parse(Int64, ARGS[2])
+seed = parse(Int64, ARGS[3])
+alternative = parse(String, ARGS[4])
+decision_function = parse(String, ARGS[5])
+iterations_until_stable =  parse(Int64, ARGS[6])
+μ = parse(Float64, ARGS[7])
+time_limit = 1800
+@show seed, dimension
+
+################# Run example #######################
+try 
+    nonlinear_boscia(mode, dimension, seed, alternative, decision_function, iterations_until_stable, μ,  time_limit)
+catch e 
+    println(e)
+    file = "nonlinear" * str(seed) * "_" * str(dimension) * "_" * mode 
+    open(file * ".txt","a") do io
+        println(io, e)
+    end
+end
