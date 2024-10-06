f8383343c38e409d871a8978883adfb27b6c5d25
Leon <stanzel.leon@gmail.com>
2024-10-06 22:19:19 +0200

    int sparse reg example

--- examples/int_sparse_reg.jl
+++ examples/int_sparse_reg.jl
@@ -1,12 +1,17 @@
-using Statistics
 using Boscia
 using FrankWolfe
+using Test
 using Random
 using SCIP
-import Bonobo
-using Test
+using HiGHS
+# using Statistics
+using LinearAlgebra
+using Distributions
+using CSV
+using DataFrames
 import MathOptInterface
 const MOI = MathOptInterface
+include("example_utilities.jl")
 
 # Integer sparse regression
 
@@ -20,36 +25,15 @@ const MOI = MathOptInterface
 # y - is the vector of results.
 # r - controls how often we have to maximal split on a index.
 # k - is the sparsity parameter. We only want a few non zero entries.
-
-# For bug hunting:
-seed = rand(UInt64)
-@show seed
-#seed = 0xeadb922ca734998b  
-Random.seed!(seed)
-
-n = 10
-m = 30
-l = 5
-k = 4
-
-sol_x = rand(1:l, n)
-for _ in 1:(n-k)
-    sol_x[rand(1:n)] = 0
-end
-
-#=k=0 # correct k
-for i in 1:n
-    if sol_x[i] == 0 
-        global k += 1
+function int_sparse_regression(o, n, m, l, k, seed)
+    Random.seed!(seed)
+    sol_x = rand(1:l, n)
+    for _ in 1:(n-k)
+        sol_x[rand(1:n)] = 0
     end
-end
-k = n-k =#
+    D = rand(m, n)
+    y_d = D * sol_x
 
-const D = rand(m, n)
-const y_d = D * sol_x
-
-@testset "Integer sparse regression" begin
-    o = SCIP.Optimizer()
     MOI.set(o, MOI.Silent(), true)
     MOI.empty!(o)
     x = MOI.add_variables(o, n)
@@ -68,7 +52,7 @@ const y_d = D * sol_x
     MOI.add_constraint(o, sum(z, init=0.0), MOI.LessThan(1.0 * k))
     # MOI.add_constraint(o, MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(zeros(n),x), sum(Float64.(iszero.(x)))), MOI.GreaterThan(1.0*(n-k)))
     # MOI.add_constraint(o, MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(ones(n),z), 0.0), MOI.GreaterThan(1.0*k))
-    lmo = FrankWolfe.MathOptLMO(o)
+    lmo = Boscia.MathOptBLMO(o)
 
     function f(x)
         xv = @view(x[1:n])
@@ -81,20 +65,34 @@ const y_d = D * sol_x
         return storage
     end
 
-    #= function perform_strong_branch(tree, node)
-         return node.level <= length(tree.root.problem.integer_variables)
-     end
-     branching_strategy = Boscia.HybridStrongBranching(10, 1e-3, HiGHS.Optimizer(), perform_strong_branch)
-     MOI.set(branching_strategy.pstrong.optimizer, MOI.Silent(), true)=#
-
-
-    x, _, result = Boscia.solve(f, grad!, lmo, verbose=true, max_fw_iter=10001, rel_dual_gap=1e-3)
-
-    val_min, x_min = Boscia.sparse_min_via_enum(f, n, k, fill(0:l, n))
-    #@show x_min
-    @show x[1:n]
-    @show x_min
-    @test val_min == f(x)
-    @test isapprox(x[1:n], x_min)
-    @test isapprox(f(x), f(result[:raw_solution]), atol=1e-6, rtol=1e-6)
+    return lmo, f, grad!
 end
+
+function int_sparse_reg_boscia(mode, dimension, seed, alternative, decision_function, iterations_until_stable, μ,  time_limit = 1800)
+    
+    o = SCIP.Optimizer()
+
+    n = dimension
+    m = 3* dimension
+    l = ceil(dimension/ 2)
+    k = l - 1
+    lmo, f, grad! = int_sparse_regression(o, n, m, l, k, seed)
+
+    branching_strategy, settings = build_branching_strategy(lmo, mode, alternative, decision_function, iterations_until_stable, μ)
+    # println(o)
+    println("precompile")
+    Boscia.solve(f, grad!, lmo, verbose=false, time_limit=10)
+   # print(lmo.o)
+    println("actual run")
+
+    x, _, result = Boscia.solve(f, grad!, lmo, verbose=true, time_limit=time_limit, branching_strategy = branching_strategy)
+
+    ### Define Parameters needed for documenting the results ###
+    example_name = "int_sparse_reg"
+    file_name = "int_sparse_reg_results"
+    # Save result
+    save_results(result, settings, μ, example_name, seed, dimension, file_name,false)
+    return result
+end
+
+#int_sparse_reg_boscia("strong_branching", 10, 1, "na", "na", 1, 1e-6, 180)
